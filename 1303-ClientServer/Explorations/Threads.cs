9dc1f245399eba96029074f4ca01d3f3e2bb74fc
Tze-Ching <ginasun28@gmail.com>
2022-05-25 14:29:36 -0700

    Add project files.

---
+++ 1303-ClientServer/Explorations/Threads.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1303_ClientServer.Explorations
+{
+    public class Threads
+    {
+
+    }
+}
---
+++ 1303-ClientServer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace _1303_ClientServer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Assignment1 a = new Assignment1();
+            a.Run();
+
+
+            //Console.WriteLine("Hello World!");
+            //var assignment1 = new Assignment1();
+            //assignment1.Run();
+
+            //TestThreads();
+
+            //Console.WriteLine();
+            //Console.WriteLine("hit enter key to terminate");
+            //Console.ReadLine();
+        }
+
+        /*
+        public static void TestThreads()
+        {
+            Thread t1 = new Thread(MyFunction);
+            t1.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write('m');
+            }
+        }
+
+        private static void MyFunction()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("f");
+            }
+        }
+        */
+    }
+}
